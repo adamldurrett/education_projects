661ee5abfc81d48cb1579a38af46aa10b754a12c
adamldurrett <adamldurrett@gmail.com>
2022-09-13 00:51:45 -0400

    new card game project

---
+++ CardGame/GameManager.java
@@ -0,0 +1,17 @@
+public class GameManager{
+	public LinkedList players;
+
+
+	public static void main(String [] args){
+		System.out.println("Ready to play?");
+		playGame();
+	}
+
+	void playGame(){
+
+	}
+
+	void addPlayer(){
+
+	}
+}
